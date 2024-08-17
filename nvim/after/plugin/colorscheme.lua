247dc84139e6aee0714eb0849b185199615af75d
Alaksiej Ščarbaty <hahaha.funny.yellow.dog@gmail.com>
2024-08-17 17:42:56 +0200

    more colorschemes

---
+++ nvim/after/plugin/colorscheme.lua
@@ -0,0 +1,8 @@
+-- vim.cmd('colorscheme kanagawa')
+
+require('onedark').setup {
+	style = 'warmer',
+}
+
+require('onedark').load()
+
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,6 @@
+return {
+	'rebelot/kanagawa.nvim',
+	'catppuccin/nvim',
+	'folke/tokyonight.nvim',
+	'navarasu/onedark.nvim',
+}
