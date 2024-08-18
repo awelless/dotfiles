266b6c4ede84f8c5bb22458c1fc631a410bd0698
Alaksiej Ščarbaty <hahaha.funny.yellow.dog@gmail.com>
2024-08-18 19:24:48 +0200

    telescope file browser

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,15 +5,27 @@ require('telescope').setup {
                 prompt_position = 'top',
             },
         },
-        path_display = {'smart'},
+        path_display = { 'smart' },
         sorting_strategy = 'ascending',
+    },
+    extensions = {
+        file_browser = {
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+        },
     },
 }
 
+-- Telescope mappings.
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+
+-- Telescope file browser.
+require('telescope').load_extension 'file_browser'
+vim.keymap.set('n', '<leader>fd', ':Telescope file_browser<cr>')
+vim.keymap.set('n', '<leader>fc', ':Telescope file_browser path=%:p:h select_buffer=true<cr>')
 
