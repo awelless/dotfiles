74d81c5382b4389aaee51fd228fc235b7305ea3a
Alaksiej Ščarbaty <hahaha.funny.yellow.dog@gmail.com>
2024-09-01 15:50:55 +0200

    add go lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -33,18 +33,19 @@ require('mason-lspconfig').setup({
 	ensure_installed = {
 		-- Add new LSPs here.
 		'clangd',
+        'gopls',
 		'jdtls',
 		'lua_ls',
 		'pyright',
 	}
 })
 
-require("mason-lspconfig").setup_handlers {
+require('mason-lspconfig').setup_handlers {
     -- The first entry (without a key) will be the default handler
     -- and will be called for each installed server that doesn't have
     -- a dedicated handler.
     function (server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {}
+        require('lspconfig')[server_name].setup {}
     end,
 }
 
