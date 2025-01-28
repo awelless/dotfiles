4daf130689ec2c5829f8111de84e66e918278c45
Alaksiej Ščarbaty <hahaha.funny.yellow.dog@gmail.com>
2025-01-28 12:21:07 +0100

    Add tpm and tmux-yank

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # Allowing for script to be executed from anywhere.
 dotfiles_dir=$(dirname $0)
 
+# Installing tmux plugin manager.
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # Creating symlinks to the dotfiles.
 ln -fsn $PWD/$dotfiles_dir/nvim ~/.config/nvim
 ln -fs $PWD/$dotfiles_dir/p10k.zsh ~/.p10k.zsh
